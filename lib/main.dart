c44be067c18db70747c55d98f17f388c251951a0
Yihun Alemayehu <yankure01@gmail.com>
2023-12-30 14:33:12 +0300

    fifth

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
+import 'package:customer_crud/customer_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
+  runApp(MaterialApp(
+    home: BlocProvider(
+      create: (context) => CustomerCubit(),
+      child: const HomePage(),
+    ),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CUSTOMER'),
+        centerTitle: true,
       ),
       body: Container(),
     );
